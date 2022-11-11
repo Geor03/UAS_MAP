cf32b1659729695c73f390f3cd32bd40fa08390b
Richard <dualuigi@gmail.com>
2022-11-11 19:08:43 +0700

    asd

---
+++ PROJECT_MAP/app/src/main/java/umn/ac/id/project_map/LaundryModel.java
@@ -0,0 +1,44 @@
+package umn.ac.id.project_map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+    public class LaundryModel {
+
+        private String laundry_name;
+//        private int laundry_rating;
+        private int laundry_image;
+
+        // Constructor
+        public LaundryModel(String laundry_name,  int laundry_image) {
+            this.laundry_name = laundry_name;
+//            this.course_rating = course_rating;
+            this.laundry_image = laundry_image;
+        }
+
+        // Getter and Setter
+        public String getLaundry_name() {
+            return laundry_name;
+        }
+
+        public void setLaundry_name(String laundry_name) {
+            this.laundry_name = laundry_name;
+        }
+
+//        public int getCourse_rating() {
+//            return course_rating;
+//        }
+
+//        public void setCourse_rating(int course_rating) {
+//            this.course_rating = course_rating;
+//        }
+
+        public int getLaundry_image() {
+            return laundry_image;
+        }
+
+        public void setCourse_image(int laundry_image) {
+            this.laundry_image = laundry_image;
+        }
+    }
