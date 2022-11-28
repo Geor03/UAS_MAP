3267e194606a90e0ee00be508b996bbf5664a47d
Ric168 <dualuigi@gmail.com>
2022-11-28 20:31:07 +0700

    editProfile
    -error firebase

--- PROJECT_MAP/app/src/main/java/umn/ac/id/project_map/Profile.java
+++ PROJECT_MAP/app/src/main/java/umn/ac/id/project_map/Profile.java
@@ -3,10 +3,17 @@ package umn.ac.id.project_map;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -14,18 +21,30 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Profile extends AppCompatActivity {
-    TextView fName, email, phone;
+    //    TextView fName, email, phone;
+    TextView email, phone;
+    EditText fName;
+
+
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    String userID;
+    String userID,_FIRSTNAME, _EMAIL, _PHONENO;
+
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        phone = findViewById(R.id.phone);
-        fName = findViewById(R.id.name);
-        email = findViewById(R.id.email);
+
+        reference = FirebaseDatabase.getInstance().getReference("users");
+
+
+        phone = (TextView) findViewById(R.id.phone);
+        fName = (EditText) findViewById(R.id.name);
+        email = (TextView) findViewById(R.id.email);
+
+//        showAllUserData();
 
 
         fAuth = FirebaseAuth.getInstance();
@@ -41,5 +60,39 @@ public class Profile extends AppCompatActivity {
                 email.setText(documentSnapshot.getString("email"));
             }
         });
+    }
+
+//    private void showAllUserData(){
+//        Intent intent = getIntent();
+//        _FIRSTNAME = intent.getStringExtra("firstName");
+//        _EMAIL = intent.getStringExtra("Email");
+//        _PHONENO = intent.getStringExtra("phoneNo");
+//
+//        email.setText(_EMAIL);
+//        phone.setText(_PHONENO);
+//        fName.setText(_FIRSTNAME);
+//    }
+
+
+    public void update(View view){
+
+        if(isNameChanged()){
+            Toast.makeText(this,"Data telah diupdate", Toast.LENGTH_LONG).show();
+        }
+        else Toast.makeText(this, "Data gagal",Toast.LENGTH_LONG).show();
+    }
+
+
+
+    public boolean isNameChanged() {
+//        if(!_FIRSTNAME.equals(fName.getEditableText().toString())){
+        if(!_FIRSTNAME.equals(fName.getText().toString())){
+            reference.child(userID).child("name").setValue(fName.getEditableText().toString());
+            _FIRSTNAME = fName.getEditableText().toString();
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
