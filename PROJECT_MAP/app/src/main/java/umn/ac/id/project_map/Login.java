5b727143f9105f9e4f69cbf30e44e6d2ac47584f
George <91233711+Geor03@users.noreply.github.com>
2022-12-18 00:40:31 +0700

    mm

--- PROJECT_MAP/app/src/main/java/umn/ac/id/project_map/Login.java
+++ PROJECT_MAP/app/src/main/java/umn/ac/id/project_map/Login.java
@@ -3,11 +3,13 @@ package umn.ac.id.project_map;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -26,8 +28,12 @@ public class Login extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     EditText mEmail, mPassword;
     CheckBox mSave;
-    Boolean agree;
-    SharedPreferences signupPreferences;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    Boolean save;
+
+    String password;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,9 @@ public class Login extends AppCompatActivity {
         mEmail = (EditText)findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         mSave = (CheckBox) findViewById(R.id.savelogin);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
         firebaseAuth = FirebaseAuth.getInstance();
 
         if(FirebaseAuth.getInstance().getCurrentUser() != null){
@@ -45,13 +54,37 @@ public class Login extends AppCompatActivity {
             finish();
         }
 
+        save = loginPreferences.getBoolean("saveLogin", false);
+        if (save == true) {
+            mEmail.setText(loginPreferences.getString("email", ""));
+            mPassword.setText(loginPreferences.getString("password", ""));
+            mSave.setChecked(true);
+        }
+
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 btnLogin.setEnabled(false);
-                String password = mPassword.getText().toString().trim();
-                String email = mEmail.getText().toString().trim();
+                password = mPassword.getText().toString().trim();
+                email = mEmail.getText().toString().trim();
+
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mEmail.getWindowToken(), 0);
+
+                email = mEmail.getText().toString();
+                password = mPassword.getText().toString();
+
+                if (mSave.isChecked()) {
+                    loginPrefsEditor.putBoolean("saveLogin", true);
+                    loginPrefsEditor.putString("username", email);
+                    loginPrefsEditor.putString("password", password);
+                    loginPrefsEditor.commit();
+                } else {
+                    loginPrefsEditor.clear();
+                    loginPrefsEditor.commit();
+                }
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
